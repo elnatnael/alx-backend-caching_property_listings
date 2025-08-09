e7b7c2b62847d38bd8c5402879dbd5a028df9fa0
elnatnael <meranat9104@gmail.com>
2025-08-09 15:03:06 +0300

    Cache Property List View

--- properties/views.py
+++ properties/views.py
@@ -5,18 +5,16 @@ from .models import Property
 @cache_page(60 * 15)  # Cache for 15 minutes
 def property_list(request):
     properties = Property.objects.all()
-    data = {"properties": []}  # Initialize data here
-    
-    for prop in properties:
-        data["properties"].append({
-            "id": prop.id,
-            "title": prop.title,
-            "description": prop.description,
-            "price": str(prop.price),
-            "location": prop.location,
-            "created_at": prop.created_at.isoformat()
-        })
-    
-    return JsonResponse(
-        data  # Return the data variable
-    )
+    return JsonResponse({
+        "properties": [
+            {
+                "id": prop.id,
+                "title": prop.title,
+                "description": prop.description,
+                "price": str(prop.price),
+                "location": prop.location,
+                "created_at": prop.created_at.isoformat()
+            }
+            for prop in properties
+        ]
+    })
